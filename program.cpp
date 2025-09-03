6dadc477c9e804d24c87370853f30a3b4f3f678e
0XL1ght <bannedbylight@gmail.com>
2025-09-04 00:18:44 +0530

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int start, end, num;
+    
+    printf("Enter start number: ");
+    scanf("%d", &start);
+    
+    printf("Enter end number: ");
+    scanf("%d", &end);
+    
+    if (start > end)
+    {
+        printf("Error: Start should be less than end\n");
+        return 0;
+    }
+    
+    printf("\nChecking odd/even in range %d to %d:\n", start, end);
+    printf("----------------------------------------\n");
+    
+    num = start;
+    while (num <= end)
+    {
+        if (num % 2 == 0)
+        {
+            printf("%d is EVEN, Square: %d\n", num, num * num);
+        }
+        else
+        {
+            printf("%d is ODD,  Cube: %d\n", num, num * num * num);
+        }
+        num++;
+    }
+    
+    return 0;
+}
